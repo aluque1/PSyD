64d9c843ab5f5c30838086bcf46aa689c37c1c02
Nachoski08 <nachosantatecla3@gmail.com>
2022-10-19 23:24:52 +0200

    Esto es horrible

--- BSP/src/system.c
+++ BSP/src/system.c
@@ -14,80 +14,80 @@
 #define SYSMODE (0x1f)
 
 #define SET_OPMODE( mode )                 \
-		asm volatile (                         \
-				"mrs r0, cpsr               \n"    \
-				"bic r0, r0, #0x1f          \n"    \
-				"orr r0, r0, %0             \n"    \
-				"msr cpsr_c, r0               "    \
-				:                                  \
-				                                   : "i" (mode)                       \
-				                                     : "r0"                             \
-		)
+    asm volatile (                         \
+        "mrs r0, cpsr               \n"    \
+        "bic r0, r0, #0x1f          \n"    \
+        "orr r0, r0, %0             \n"    \
+        "msr cpsr_c, r0               "    \
+        :                                  \
+        : "i" (mode)                       \
+        : "r0"                             \
+    )
 
 #define GET_OPMODE( mode_p )               \
-		asm volatile (                         \
-				"mrs r0, cpsr               \n"    \
-				"and r0, r0, #0x1f          \n"    \
-				"strb r0, %0                  "    \
-				: "=m" (*mode_p)                   \
-				  :                                  \
-				                                     : "r0"                             \
-		);
+    asm volatile (                         \
+        "mrs r0, cpsr               \n"    \
+        "and r0, r0, #0x1f          \n"    \
+        "strb r0, %0                  "    \
+        : "=m" (*mode_p)                   \
+        :                                  \
+        : "r0"                             \
+    );
 
 #define SET_IRQFLAG( value )               \
-		asm volatile (                         \
-				"mrs r0, cpsr               \n"    \
-				"bic r0, r0, #0x80          \n"    \
-				"orr r0, r0, %0             \n"    \
-				"msr cpsr_c, r0               "    \
-				:                                  \
-				                                   : "i" (value<<7)                   \
-				                                     : "r0"                             \
-		)
+    asm volatile (                         \
+        "mrs r0, cpsr               \n"    \
+        "bic r0, r0, #0x80          \n"    \
+        "orr r0, r0, %0             \n"    \
+        "msr cpsr_c, r0               "    \
+        :                                  \
+        : "i" (value<<7)                   \
+        : "r0"                             \
+    )
 
-#define SET_FIQFLAG( value )               \
-		asm volatile (                           \
-				"mrs r0, cpsr                  \n"     \
-				"bic r0, r0, #0x40             \n"     \
-				"orr r0, r0, %0                \n"     \
-				"msr cpsr_c, r0                  "     \
-				:                                      \
-				                                       : "i" (value<<6)                       \
-				                                         : "r0"                                 \
-		)
+#define SET_FIQFLAG( value )              \
+    asm volatile (                        \
+        "mrs r0, cpsr               \n"   \
+        "bic r0, r0, #0x40          \n"   \
+        "orr r0, r0, %0             \n"   \
+        "msr cpsr_c, r0               "   \
+        :                                 \
+        : "i" (value<<6)                  \
+        : "r0"                            \
+    )
 
-void isr_SWI_dummy( void)        __attribute__ ((interrupt ("SWI")));
+void isr_SWI_dummy( void )       __attribute__ ((interrupt ("SWI")));
 void isr_UNDEF_dummy( void )     __attribute__ ((interrupt ("UNDEF")));
 void isr_IRQ_dummy( void )       __attribute__ ((interrupt ("IRQ")));
 void isr_FIQ_dummy( void )       __attribute__ ((interrupt ("FIQ")));
 void isr_PABORT_dummy( void )    __attribute__ ((interrupt ("ABORT")));
 void isr_DABORT_dummy( void )    __attribute__ ((interrupt ("ABORT")));
-void isr_ADC_dummy( void )       __attribute__ ((interrupt ("IRQ")));
-void isr_RTC_dummy( void )       __attribute__ ((interrupt ("IRQ")));
-void isr_UTXD1_dummy( void ) 	 __attribute__ ((interrupt ("IRQ")));
-void isr_UTXD0_dummy( void )	 __attribute__ ((interrupt ("IRQ")));
-void isr_SIO_dummy( void )		 __attribute__ ((interrupt ("IRQ")));
-void isr_IIC_dummy( void )		 __attribute__ ((interrupt ("IRQ")));
-void isr_URXD1_dummy( void )	 __attribute__ ((interrupt ("IRQ")));
-void isr_URXD0_dummy( void )	 __attribute__ ((interrupt ("IRQ")));
-void isr_TIMER5_dummy( void )	 __attribute__ ((interrupt ("IRQ")));
-void isr_TIMER4_dummy( void )	 __attribute__ ((interrupt ("IRQ")));
-void isr_TIMER3_dummy( void )	 __attribute__ ((interrupt ("IRQ")));
-void isr_TIMER2_dummy( void )	 __attribute__ ((interrupt ("IRQ")));
-void isr_TIMER1_dummy( void )	 __attribute__ ((interrupt ("IRQ")));
-void isr_TIMER0_dummy( void )	 __attribute__ ((interrupt ("IRQ")));
-void isr_UERR01_dummy( void )	 __attribute__ ((interrupt ("IRQ")));
-void isr_WDT_dummy( void )		 __attribute__ ((interrupt ("IRQ")));
-void isr_BDMA1_dummy( void )	 __attribute__ ((interrupt ("IRQ")));
-void isr_BDMA0_dummy( void )	 __attribute__ ((interrupt ("IRQ")));
-void isr_ZDMA1_dummy( void )	 __attribute__ ((interrupt ("IRQ")));
-void isr_ZDMA0_dummy( void )	 __attribute__ ((interrupt ("IRQ")));
-void isr_TICK_dummy( void )		 __attribute__ ((interrupt ("IRQ")));
-void isr_PB_dummy( void )		 __attribute__ ((interrupt ("IRQ")));
-void isr_ETHERNET_dummy( void )	 __attribute__ ((interrupt ("IRQ")));
-void isr_TS_dummy( void )		 __attribute__ ((interrupt ("IRQ")));
-void isr_KEYPAD_dummy( void )	 __attribute__ ((interrupt ("IRQ")));
-void isr_USB_dummy( void )       __attribute__ ((interrupt ("IRQ")));
+void isr_ADC_dummy( void )       __attribute__ ((interrupt ("ADC")));
+void isr_RTC_dummy( void )       __attribute__ ((interrupt ("RTC")));
+void isr_UTXD1_dummy( void ) 	 __attribute__ ((interrupt ("UTXD")));
+void isr_UTXD0_dummy( void )	 __attribute__ ((interrupt ("UTXD")));
+void isr_SIO_dummy( void )		 __attribute__ ((interrupt ("SIO")));
+void isr_IIC_dummy( void )		 __attribute__ ((interrupt ("IIC")));
+void isr_URXD1_dummy( void )	 __attribute__ ((interrupt ("URXD")));
+void isr_URXD0_dummy( void )	 __attribute__ ((interrupt ("URXD")));
+void isr_TIMER5_dummy( void )	 __attribute__ ((interrupt ("TIMER")));
+void isr_TIMER4_dummy( void )	 __attribute__ ((interrupt ("TIMER")));
+void isr_TIMER3_dummy( void )	 __attribute__ ((interrupt ("TIMER")));
+void isr_TIMER2_dummy( void )	 __attribute__ ((interrupt ("TIMER")));
+void isr_TIMER1_dummy( void )	 __attribute__ ((interrupt ("TIMER")));
+void isr_TIMER0_dummy( void )	 __attribute__ ((interrupt ("TIMER")));
+void isr_UERR01_dummy( void )	 __attribute__ ((interrupt ("UERR01")));
+void isr_WDT_dummy( void )		 __attribute__ ((interrupt ("WDT")));
+void isr_BDMA1_dummy( void )	 __attribute__ ((interrupt ("BDMA")));
+void isr_BDMA0_dummy( void )	 __attribute__ ((interrupt ("BDMA")));
+void isr_ZDMA1_dummy( void )	 __attribute__ ((interrupt ("ZDMA")));
+void isr_ZDMA0_dummy( void )	 __attribute__ ((interrupt ("ZDMA")));
+void isr_TICK_dummy( void )		 __attribute__ ((interrupt ("TICK")));
+void isr_PB_dummy( void )		 __attribute__ ((interrupt ("PB")));
+void isr_ETHERNET_dummy( void )	 __attribute__ ((interrupt ("ETHERNET")));
+void isr_TS_dummy( void )		 __attribute__ ((interrupt ("TS")));
+void isr_KEYPAD_dummy( void )	 __attribute__ ((interrupt ("KEYPAD")));
+void isr_USB_dummy( void )       __attribute__ ((interrupt ("USB")));
 
 void sys_init( void )
 {
@@ -114,7 +114,7 @@ void sys_init( void )
 	INTMOD = 0x0;
 	install_dummy_isr();
 	EXTINTPND = 0x0;
-	I_ISPC = ...;
+	I_ISPC = 0x1;
 	INTCON = 0x1;
 	SET_IRQFLAG( 0 );
 	SET_FIQFLAG( 1 );
@@ -139,7 +139,7 @@ static void port_init( void )
 	PUPC  = 0x94FB;
 
 	PDATD = ~0;
-	PCOND =  0xAAAA;
+	PCOND = 0xAAAA;
 	PUPD  = 0xFF;
 
 	PDATE = ~0;
@@ -147,12 +147,12 @@ static void port_init( void )
 	PUPE  = 0xFB;
 
 	PDATF = ~0;
-	PCONF =  0x251A;
+	PCONF = 0x251A;
 	PUPF  = 0x74;
 
 	PDATG = ~0;
 	PCONG = 0xF5FF;
-	PUPG  =  0x30;
+	PUPG  = 0x30;
 
 	SPUCR = 0x7;
 
@@ -161,9 +161,38 @@ static void port_init( void )
 
 static void install_dummy_isr( void )
 {
-	...
-	pISR_TICK = (uint32) isr_TICK_dummy;
-	...
+	pISR_SWI =		(uint32) isr_SWI_dummy;
+	pISR_UNDEF =	(uint32) isr_UNDEF_dummy;
+	pISR_IRQ =		(uint32) isr_IRQ_dummy;
+	pISR_FIQ =		(uint32) isr_FIQ_dummy;
+	pISR_PABORT =	(uint32) isr_PABORT_dummy;
+	pISR_DABORT = 	(uint32) isr_DABORT_dummy;
+	pISR_ADC = 		(uint32) isr_ADC_dummy;
+	pISR_RTC =		(uint32) isr_RTC_dummy;
+	pISR_UTXD1 =	(uint32) isr_UTXD1_dummy;
+	pISR_UTXD0 =	(uint32) isr_UTXD0_dummy;
+	pISR_SIO =		(uint32) isr_SIO_dummy;
+	pISR_IIC = 		(uint32) isr_IIC_dummy;
+	pISR_URXD1 = 	(uint32) isr_URXD1_dummy;
+	pISR_URXD0 =	(uint32) isr_URXD0_dummy;
+	pISR_TIMER5 =	(uint32) isr_TIMER5_dummy;
+	pISR_TIMER4 =	(uint32) isr_TIMER4_dummy;
+	pISR_TIMER3 =	(uint32) isr_TIMER3_dummy;
+	pISR_TIMER2 =	(uint32) isr_TIMER2_dummy;
+	pISR_TIMER1 =	(uint32) isr_TIMER1_dummy;
+	pISR_TIMER0 =	(uint32) isr_TIMER0_dummy;
+	pISR_UERR01 =	(uint32) isr_UERR01_dummy;
+	pISR_WDT =		(uint32) isr_WDT_dummy;
+	pISR_BDMA1 =	(uint32) isr_BDMA1_dummy;
+	pISR_BDMA0 =	(uint32) isr_BDMA0_dummy;
+	pISR_ZDMA1 =	(uint32) isr_ZDMA1_dummy;
+	pISR_ZDMA0 =	(uint32) isr_ZDMA0_dummy;
+	pISR_TICK =		(uint32) isr_TICK_dummy;
+	pISR_PB =		(uint32) isr_PB_dummy;
+	pISR_ETHERNET = (uint32) isr_ETHERNET_dummy;
+	pISR_TS =		(uint32) isr_TS_dummy;
+	pISR_KEYPAD =	(uint32) isr_KEYPAD_dummy;
+	pISR_USB =		(uint32) isr_USB_dummy;
 }        
 
 void isr_SWI_dummy( void )
@@ -392,7 +421,7 @@ void isr_USB_dummy( void )
 
 static void show_sys_info( void )
 {
-	...
+	//TODO
 }
 
 inline void sleep( void )
