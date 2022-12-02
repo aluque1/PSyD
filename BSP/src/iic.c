2503a2da9b679a498b2a35dc3330d7f2b6495fdb
Aluque <32274359+aluque1@users.noreply.github.com>
2022-12-02 18:33:27 +0100

    creo que esta
    No lo he llegado a probar que me tengo que pelear con los drivers apara ver si funciona, pero en un principio deberia estar

--- BSP/src/iic.c
+++ BSP/src/iic.c
@@ -1,4 +1,4 @@
-/*
+
 #include <s3c44b0x.h>
 #include <s3cev40.h>
 #include <timers.h>
@@ -6,37 +6,50 @@
 
 void iic_init( void )
 {
-    ...
+    IICADD = 0x0; // Direccion como escalavo, no tiene direccion porque nunca se va a ver como esclavo
+        /**
+        *  IICCON[6] = 0    :: IICCLK = MCLK/16
+        *  IICCON[3:0] = 15 :: TxCLK = IICCLK / (15+1) = 64MHz / 256
+        */
+    IICCON = 0xAF; // Establecemos la frecuencia de transmision a 250KHz
+        /**
+        *  IICSTAT[7:6] = XX :: La seleccion de modo que en el init nos da igual
+        *  IICSTAT[4] = 15   :: Serial output enable
+        */
+    IICSTAT = 0X10; // Modo de transmision, en este caso habilitamos la lectura/escritura
 }
 
 void iic_start( uint8 mode, uint8 byte )
 {
-    IICDS   = ...;
-    IICSTAT = ...;
-    IICCON &= ...;
-    while( ... );
+    /**
+     * Como estamos comenzando el iic tenemos que primer tenemos que iniciar la transmision (tenemos que crear una transicion de 1 a 0 en SDA (esto es el start cond))
+    */
+    IICDS   = byte; // Escribimos el dato en el registro de transmision
+    IICSTAT = (mode | 0x20);// Escribimos un 1 en ICCSTAT[5] (esto genera el start condition) y ponemos el modo IICSTAT[7:6] como se pasa por arg
+    IICCON &= ~(1 << 4); // Arrancamos la transmision del dato (escribir 0 en IICCON[4])
+    while((IICCON & 0x10) != 0x10); // Espermos a que termine la transmision (recepcion de ACK)
 }
 
 
 void iic_putByte( uint8 byte )
 {
-    IICDS   = ...;
-    IICCON &= ...;
-    while( ... );    
+    IICDS   = byte;
+    IICCON &= ~(1 << 4);
+    while((IICCON & 0x10) != 0x10);    
 }
 
 uint8 iic_getByte( uint8 ack )
 {
-    IICCON  = ...;
-    IICCON &= ...;
-    while( ... );
-    return ...;
+    IICCON  = ack;
+    IICCON &= ~(1 << 4);
+    //TODO no se si los whiles estan bien pero creo que si
+    while((IICCON & 0x10) != 0x10); // espera la recepcion de un dato 
+    return IICDS;
 }
 
 void iic_stop( uint16 ms )
 {
-    IICSTAT &= ...;
-    IICCON  &= ...;
+    IICSTAT &= ~(1 << 5); // genera stop condition IICSTAT[5] = 0
+    IICCON  &= ~(1 << 4); // arranca la tranmision del bit de stop
     sw_delay_ms( ms );
 }
-*/
