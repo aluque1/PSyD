4a5d960362dd3a0445040ad9e6883b09e15a1ca6
Aluque <32274359+aluque1@users.noreply.github.com>
2022-12-05 11:34:00 +0100

    update de ack while y mascara

--- BSP/src/iic.c
+++ BSP/src/iic.c
@@ -14,7 +14,7 @@ void iic_init( void )
     IICCON = 0xAF; // Establecemos la frecuencia de transmision a 250KHz
         /**
         *  IICSTAT[7:6] = XX :: La seleccion de modo que en el init nos da igual
-        *  IICSTAT[4] = 15   :: Serial output enable
+        *  IICSTAT[4] = 1    :: Serial output enable
         */
     IICSTAT = 0X10; // Modo de transmision, en este caso habilitamos la lectura/escritura
 }
@@ -25,9 +25,9 @@ void iic_start( uint8 mode, uint8 byte )
      * Como estamos comenzando el iic tenemos que primer tenemos que iniciar la transmision (tenemos que crear una transicion de 1 a 0 en SDA (esto es el start cond))
     */
     IICDS   = byte; // Escribimos el dato en el registro de transmision
-    IICSTAT = (mode | 0x20);// Escribimos un 1 en ICCSTAT[5] (esto genera el start condition) y ponemos el modo IICSTAT[7:6] como se pasa por arg
+    IICSTAT = (((mode & 0x3) << 6)| 0x20);// Escribimos un 1 en ICCSTAT[5] (esto genera el start condition) y ponemos el modo IICSTAT[7:6] como se pasa por arg
     IICCON &= ~(1 << 4); // Arrancamos la transmision del dato (escribir 0 en IICCON[4])
-    while((IICCON & 0x10) != 0x10); // Espermos a que termine la transmision (recepcion de ACK)
+    while((IICSTAT & 0)); // Espermos a que termine la transmision (recepcion de ACK)
 }
 
 
@@ -35,15 +35,15 @@ void iic_putByte( uint8 byte )
 {
     IICDS   = byte;
     IICCON &= ~(1 << 4);
-    while((IICCON & 0x10) != 0x10);    
+    while((IICSTAT & 0));    
 }
 
 uint8 iic_getByte( uint8 ack )
 {
-    IICCON  = ack;
+    IICCON  = (ack << 7);
     IICCON &= ~(1 << 4);
     //TODO no se si los whiles estan bien pero creo que si
-    while((IICCON & 0x10) != 0x10); // espera la recepcion de un dato 
+    while((IICSTAT & 0)); // espera la recepcion de un dato 
     return IICDS;
 }
 
