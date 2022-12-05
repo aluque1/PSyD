c4de152ca93cbeac87206feebf055fa923b66866
Aluque <32274359+aluque1@users.noreply.github.com>
2022-12-05 12:46:53 +0100

    Update at24C04.c
    casi terminado jej

--- BSP/src/at24C04.c
+++ BSP/src/at24C04.c
@@ -1,4 +1,4 @@
-/*
+
 #include <s3c44b0x.h>
 #include <at24c04.h>
 #include <iic.h>
@@ -10,7 +10,7 @@
 
 void at24c04_clear( void )
 {
-    ...
+    // TODO hacer el clear de la EEPROM
 }
 
 void at24c04_bytewrite( uint16 addr, uint8 data )
@@ -27,16 +27,54 @@ void at24c04_bytewrite( uint16 addr, uint8 data )
     
 void at24c04_byteread( uint16 addr, uint8 *data )
 {
-    ...
+    uint8 page;
+    
+    page = (addr & 0x100) >> 8; 
+
+    iic_start(IIC_Tx, DEVICE_ADDR | (page << 1) | WRITE);
+    iic_putByte(addr & 0xFF);
+    iic_start(IIC_Tx, DEVICE_ADDR | (page << 1) | READ);
+    data = iic_getByte(0);
+
 }
 
 void at24c04_load( uint8 *buffer )
 {
-    ...
+    uint16 addr = 0;
+    uint8 page = 0;
+    uint16 index = 0;
+
+    for (int i = 0; i < 32; ++i)
+    {
+        page = addr >> 8;
+        iic_start(IIC_Tx, DEVICE_ADDR | (page << 1) | WRITE);
+        iic_putByte(addr & 0xFF);
+        iic_start(IIC_Tx, DEVICE_ADDR | (page << 1) | READ);
+        for (int j = 0; j < 16; j++)
+        {
+            buffer[index] = iic_getByte(j < 15);
+            ++index;
+        }
+        iic_stop( 5 );   
+    }
 }
 
 void at24c04_store( uint8 *buffer )
 { 
-    ...
+    uint16 addr = 0;
+    uint8 page = 0;
+    uint16 index = 0;
+
+    for (int i = 0; i < 32; ++i)
+    {
+        page = addr >> 8;
+        iic_start(IIC_Tx, DEVICE_ADDR | (page << 1) | WRITE);
+        iic_putByte(addr & 0xFF);
+        for (int j = 0; j < 16; j++)
+        {
+            iic_putByte(buffer[index]);
+            ++index;
+        }
+        iic_stop( 5 );   
+    }
 }
-*/
