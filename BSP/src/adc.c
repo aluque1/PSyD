2833235c9d3273d5474a295cbdb9112d140e8323
Nachoski08 <nachosantatecla3@gmail.com>
2022-12-15 21:34:49 +0100

    Práctica 10 completada

--- BSP/src/adc.c
+++ BSP/src/adc.c
@@ -1,4 +1,3 @@
-/*
 #include <s3c44b0x.h>
 #include <s3cev40.h>
 #include <timers.h>
@@ -10,25 +9,26 @@ extern void isr_ADC_dummy( void );
 
 void adc_init( void )
 {
-    ADCPSR = ...;
+    ADCPSR = 19;
     adc_off();
 }
 
 void adc_on( void )
 {
-    ADCCON &= ...;
+    ADCCON &= ~(1 << 5);
     sw_delay_ms( 10 );
     state = ON;
 }
 
 void adc_off( void )
 {
-    ...
+    ADCCON |= (1 << 5);
+    state = OFF;
 }
 
 uint8 adc_status( void )
 {
-    ...
+    return state;
 }
 
 uint16 adc_getSample( uint8 ch )
@@ -36,13 +36,13 @@ uint16 adc_getSample( uint8 ch )
     uint32 sample;
     uint8 i;
     
-    ADCCON = ...;
+    ADCCON = (1 << 0) | (0 << 1) | (ch & 0x3) << 2;
     sw_delay_ms( 10 );
     for( i=0, sample=0; i<5; i++ )
     {
-        ADCCON |= ...;   
-        while( ... );
-        while( ... );
+        ADCCON |= (1 << 0);   
+        while(ADCCON & (1 << 0));
+        while(!(ADCCON & (1 << 6)));
         sample += ADCDAT & 0x3ff;
     }
     return sample / 5;
@@ -50,11 +50,13 @@ uint16 adc_getSample( uint8 ch )
 
 void adc_open( void (*isr)(void) )
 {
-    ...
+    pISR_ADC = (uint32)isr;
+    I_ISPC   = BIT_ADC;
+    INTMSK  &= ~(BIT_GLOBAL | BIT_ADC );
 }
 
 void adc_close( void )
 {
-    ...
+    INTMSK  |= BIT_ADC;
+    pISR_ADC = (uint32)isr_ADC_dummy;
 }
-*/
--- BSP/src/ts.c
+++ BSP/src/ts.c
@@ -1,4 +1,3 @@
-/*
 #include <s3c44b0x.h>
 #include <s3cev40.h>
 #include <timers.h>
@@ -26,7 +25,7 @@ void ts_init( void )
     timers_init();  
     lcd_init();
     adc_init();
-    PDATE = ...;
+    PDATE = (1 << 4) | (1 << 5) | (0 << 6) | (1 << 7);
     sw_delay_ms( 1 );
     ts_on();
     ts_calibrate();
@@ -35,22 +34,24 @@ void ts_init( void )
 
 void ts_on( void )
 {
-    ...
+    adc_on();
+    state = ON;
 }
 
 void ts_off( void )
 {
-    ...
+    adc_off();
+    state = OFF;
 }
 
 uint8 ts_status( void )
 {
-    ...
+    return state;
 }
 
 uint8 ts_pressed( void )
 {
-    ...
+    return (PDATG & (1 << 2)) ? 0 : 1;
 }
 
 static void ts_calibrate( void )
@@ -59,56 +60,95 @@ static void ts_calibrate( void )
     
     lcd_on();
     do {    
-        ...
+        // BORRAR PANTALLA
+        lcd_clear();
+        // MOSTRAR MENSAJE DE CALIBRACION
+        lcd_puts(100, 112, BLACK, "Calibrando...");
+        sw_delay_s(2);
+        //PINTA CUADRADO 5X5 PX EN (0,0) Y SOLICITA PRESIONAR TOUCHSCREEN
+        lcd_clear();
+        lcd_puts(80, 128, BLACK, "Presione en el cuadrado");
+        lcd_draw_box(0, 0, 5, 5, BLACK, 1);
     
-        while( ... );
+        while(!ts_pressed()); // ESPERAMOS PRESION DE TOUCHSCREEN
         sw_delay_ms( TS_DOWN_DELAY );
         ts_scan( &Vxmin, &Vymax );
-        while( ... );
+        while( ts_pressed() ); // ESPERAMOS DEPRESION DE TOUCHSCREEN
         sw_delay_ms( TS_UP_DELAY );
 
-        ...    
+        //PINTA CUADRADO 5X5 PX EN (319,239) Y SOLICITA PRESIONAR TOUCHSCREEN
+        lcd_clear();
+        lcd_puts(80, 128, BLACK, "Presione en el cuadrado");
+        lcd_draw_box(319-5, 239-5, 319, 239, BLACK, 1);
            
-        while( ... );
+        while(!ts_pressed()); // ESPERAMOS PRESION DE TOUCHSCREEN
         sw_delay_ms( TS_DOWN_DELAY );
         ts_scan( &Vxmax, &Vymin );
-        while( ... ); 
+        while( ts_pressed() ); // ESPERAMOS DEPRESION DE TOUCHSCREEN
         sw_delay_ms( TS_UP_DELAY );
     
-        ...
-    
-        ts_getpos( &x, &y );      
+        //PINTA CUADRADO 5X5 PX EN (160,120) Y SOLICITA PRESIONAR TOUCHSCREEN
+        lcd_clear();
+        lcd_puts(80, 128, BLACK, "Presione en el cuadrado");
+        lcd_draw_box(158, 118, 163, 123, BLACK, 1);
 
-        ...
-    
+        ts_getpos( &x, &y );
+
     } while( (x > LCD_WIDTH/2+PX_ERROR) || (x < LCD_WIDTH/2-PX_ERROR) || (y > LCD_HEIGHT/2+PX_ERROR) || (y < LCD_HEIGHT/2-PX_ERROR) );
     lcd_clear();
 }
 
 void ts_getpos( uint16 *x, uint16 *y )
 {
-    ...
+    uint16 Vx, Vy;
+    while ( !ts_pressed() );
+    sw_delay_ms( TS_DOWN_DELAY );
+    ts_scan( &Vx, &Vy );
+    while ( ts_pressed() );
+    sw_delay_ms( TS_UP_DELAY );
+    ts_sample2coord( Vx, Vy, x, y );
 }
 
 void ts_getpostime( uint16 *x, uint16 *y, uint16 *ms )
 {
-    ...    
+    uint16 Vx, Vy;
+    while ( !ts_pressed() );
+    timer3_start();
+    sw_delay_ms( TS_DOWN_DELAY );
+    ts_scan( &Vx, &Vy );
+
+    while ( ts_pressed() );
+    *ms = timer3_stop() / 10;
+    sw_delay_ms( TS_UP_DELAY );
+    ts_sample2coord( Vx, Vy, x, y );
 }
 
 uint8 ts_timeout_getpos( uint16 *x, uint16 *y, uint16 ms )
 {
-    ...    
+    uint16 Vx, Vy;
+    timer3_start();
+    while ( !ts_pressed() )
+    {
+        if (timer3_timeout())
+            return TS_TIMEOUT;
+    }
+    sw_delay_ms( TS_DOWN_DELAY );
+    ts_scan( &Vx, &Vy );
+    while ( ts_pressed() );
+    sw_delay_ms( TS_UP_DELAY );
+    ts_sample2coord( Vx, Vy, x, y );
+    return TS_OK; 
 }
 
 static void ts_scan( uint16 *Vx, uint16 *Vy )
 {
-    PDATE = ...;
-    *Vx = adc_getSample( ... );
+    PDATE = (0 << 4) | (1 << 5) | (1 << 6) | (0 << 7); // X- tierra, X+ VDD, resto abiertos
+    *Vx = adc_getSample(1);
     
-    PDATE = ...;
-    *Vy = adc_getSample( ... );
+    PDATE = (1 << 4) | (0 << 5) | (0 << 6) | (1 << 7); // Y- tierra, Y+ VDD, resto abiertos
+    *Vy = adc_getSample(0);
     
-    PDATE = ...;
+    PDATE = (1 << 4) | (1 << 5) | (0 << 6) | (1 << 7); // Y- tierra, resto abiertos
     sw_delay_ms( 1 );
 }
 
@@ -119,17 +159,25 @@ static void ts_sample2coord( uint16 Vx, uint16 Vy, uint16 *x, uint16 *y )
     else if( Vx > Vxmax )
         *x = LCD_WIDTH-1;
     else 
-        *x = LCD_WIDTH*(Vx-Vxmin) / (Vxmax-Vxmin);    
-    ...
+        *x = LCD_WIDTH*(Vx-Vxmin) / (Vxmax-Vxmin);
+    
+    if( Vy < Vymin )
+        *y = LCD_HEIGHT-1;
+    else if( Vy > Vymax )
+        *y = 0;
+    else 
+        *y = LCD_HEIGHT*(Vymax - Vy) / (Vymax-Vymin);
 }
 
 void ts_open( void (*isr)(void) )
 {
-    ...
+    pISR_TS = (uint32)isr;
+    INTPND &= ~(BIT_EINT2);
+    INTMSK &= ~(BIT_GLOBAL | BIT_EINT2);
 }
 
 void ts_close( void )
 {
-    ...
+    INTMSK |= BIT_EINT2;
+    pISR_TS = (uint32)isr_TS_dummy;
 }
-*/
