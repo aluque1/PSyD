f23321576365ff707991c3502c8e5a97f723e42e
Nachoski08 <nachosantatecla3@gmail.com>
2022-12-17 19:21:11 +0100

    Update iic.c

--- BSP/src/iic.c
+++ BSP/src/iic.c
@@ -25,9 +25,9 @@ void iic_start( uint8 mode, uint8 byte )
      * Como estamos comenzando el iic tenemos que primer tenemos que iniciar la transmision (tenemos que crear una transicion de 1 a 0 en SDA (esto es el start cond))
     */
     IICDS   = byte; // Escribimos el dato en el registro de transmision
-    IICSTAT = (((mode & 0x3) << 6)| 0x20);// Escribimos un 1 en ICCSTAT[5] (esto genera el start condition) y ponemos el modo IICSTAT[7:6] como se pasa por arg
+    IICSTAT = (((mode & 0x3) << 6)| (1 << 5));// Escribimos un 1 en ICCSTAT[5] (esto genera el start condition) y ponemos el modo IICSTAT[7:6] como se pasa por arg
     IICCON &= ~(1 << 4); // Arrancamos la transmision del dato (escribir 0 en IICCON[4])
-    while(IICCON & (1 << 4)); // Espermos a que termine la transmision TODO por si acaso no va hacer (IICCON & (1 << 4)) == 1
+    while(IICSTAT & 1); // Espermos a que termine la transmision TODO por si acaso no va hacer (IICCON & (1 << 4)) == 1
 }
 
 
@@ -35,7 +35,7 @@ void iic_putByte( uint8 byte )
 {
     IICDS   = byte;
     IICCON &= ~(1 << 4);
-    while(IICCON & (1 << 4));    
+    while(IICSTAT & 1);    
 }
 
 uint8 iic_getByte( uint8 ack )
@@ -44,7 +44,7 @@ uint8 iic_getByte( uint8 ack )
     IICCON |= ((ack & 1) << 7); // ponemos el primer bit de ack en el bit 7
 
     IICCON &= ~(1 << 4);
-    while(IICCON & (1 << 4)); // espera la recepcion de un dato 
+    while(IICSTAT & 1); // espera la recepcion de un dato 
     return IICDS;
 }
 
